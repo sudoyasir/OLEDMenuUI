ce788fa632061758e763981841e583010b9263e8
🚀 Yasir N. <y451rmahar@gmail.com>
2025-08-04 15:39:42 +0500

    Add padding and border radius to selector

--- src/MenuUI.cpp
+++ src/MenuUI.cpp
@@ -1,103 +1,138 @@
 #include "MenuUI.h"
 
-MenuUI::MenuUI(Adafruit_SSD1306* disp, int up, int down, int select, int back, int buzzer)
-  : display(disp), buttonUpPin(up), buttonDownPin(down), buttonSelectPin(select),
-    buttonBackPin(back), buzzerPin(buzzer), selectedItem(0), topItem(0), itemCount(0),
-    menuItems(nullptr), callback(nullptr), backCallback(nullptr) {}
+MenuUI::MenuUI(Adafruit_SSD1306 *disp, int up, int down, int select, int back, int buzzer)
+    : display(disp), buttonUpPin(up), buttonDownPin(down), buttonSelectPin(select),
+      buttonBackPin(back), buzzerPin(buzzer), selectedItem(0), topItem(0), itemCount(0),
+      menuItems(nullptr), callback(nullptr), backCallback(nullptr), beepEnabled(true) {}
 
-void MenuUI::begin() {
+void MenuUI::begin()
+{
   pinMode(buttonUpPin, INPUT_PULLUP);
   pinMode(buttonDownPin, INPUT_PULLUP);
   pinMode(buttonSelectPin, INPUT_PULLUP);
   pinMode(buttonBackPin, INPUT_PULLUP);
-  if (buzzerPin >= 0) pinMode(buzzerPin, OUTPUT);
+  if (buzzerPin >= 0)
+    pinMode(buzzerPin, OUTPUT);
 
   display->clearDisplay();
   display->display();
 }
 
-void MenuUI::setMenuItems(const char** items, int count) {
+void MenuUI::setMenuItems(const char **items, int count, bool resetSelection)
+{
   menuItems = items;
   itemCount = count;
-  selectedItem = 0;
-  topItem = 0;
+  if (resetSelection)
+  {
+    selectedItem = 0;
+    topItem = 0;
+  }
   drawMenu();
 }
 
-void MenuUI::setCallback(void (*cb)(int)) {
+void MenuUI::setCallback(void (*cb)(int))
+{
   callback = cb;
 }
 
-void MenuUI::setBackCallback(void (*cb)()) {
+void MenuUI::setBackCallback(void (*cb)())
+{
   backCallback = cb;
 }
 
-void MenuUI::setBeepEnabled(bool enabled) {
+void MenuUI::setBeepEnabled(bool enabled)
+{
   beepEnabled = enabled;
 }
 
-void MenuUI::setBrightnessLevel(uint8_t level) {
+void MenuUI::setBrightnessLevel(uint8_t level)
+{
   display->ssd1306_command(SSD1306_SETCONTRAST);
   display->ssd1306_command(level);
 }
 
-void MenuUI::update() {
-  if (digitalRead(buttonUpPin) == LOW) {
+void MenuUI::update()
+{
+  if (digitalRead(buttonUpPin) == LOW)
+  {
     beep();
     moveUp();
     delay(200);
   }
-  if (digitalRead(buttonDownPin) == LOW) {
+  if (digitalRead(buttonDownPin) == LOW)
+  {
     beep();
     moveDown();
     delay(200);
   }
-  if (digitalRead(buttonSelectPin) == LOW) {
+  if (digitalRead(buttonSelectPin) == LOW)
+  {
     beep(1);
-    if (callback) callback(selectedItem);
+    if (callback)
+      callback(selectedItem);
     delay(200);
   }
-  if (digitalRead(buttonBackPin) == LOW) {
+  if (digitalRead(buttonBackPin) == LOW)
+  {
     beep();
-    if (backCallback) backCallback();
+    if (backCallback)
+      backCallback();
     delay(200);
   }
 }
 
-void MenuUI::moveUp() {
-  if (selectedItem > 0) {
+void MenuUI::moveUp()
+{
+  if (selectedItem > 0)
+  {
     selectedItem--;
-    if (selectedItem < topItem) topItem--;
+    if (selectedItem < topItem)
+      topItem--;
     drawMenu();
   }
 }
 
-void MenuUI::moveDown() {
-  if (selectedItem < itemCount - 1) {
+void MenuUI::moveDown()
+{
+  if (selectedItem < itemCount - 1)
+  {
     selectedItem++;
-    if (selectedItem >= topItem + maxVisibleItems) topItem++;
+    if (selectedItem >= topItem + maxVisibleItems)
+      topItem++;
     drawMenu();
   }
 }
 
-void MenuUI::drawMenu() {
+void MenuUI::drawMenu()
+{
   display->clearDisplay();
   display->setTextSize(1);
   display->setTextColor(SSD1306_WHITE);
-  display->setCursor(0, 0);
 
-  for (int i = 0; i < maxVisibleItems; ++i) {
+  int paddingX = 6;
+  int paddingY = 4;
+  int selectorHeight = 16;
+  int cornerRadius = 4;
+
+  for (int i = 0; i < maxVisibleItems; ++i)
+  {
     int index = topItem + i;
-    if (index >= itemCount) break;
+    if (index >= itemCount)
+      break;
 
-    if (index == selectedItem) {
-      display->fillRect(0, i * 16, 128, 16, SSD1306_WHITE);
-      display->setTextColor(SSD1306_BLACK);
-    } else {
+    int y = i * selectorHeight;
+
+    if (index == selectedItem)
+    {
+      display->fillRoundRect(0, y, display->width(), selectorHeight, cornerRadius, SSD1306_WHITE);
+      display->setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+    }
+    else
+    {
       display->setTextColor(SSD1306_WHITE);
     }
 
-    display->setCursor(2, i * 16 + 4);
+    display->setCursor(paddingX, y + paddingY);
     display->print(menuItems[index]);
   }
 
@@ -110,13 +145,18 @@ void MenuUI::drawMenu() {
   display->display();
 }
 
-void MenuUI::beep(int type) {
-  if (!beepEnabled || buzzerPin < 0) return;
+void MenuUI::beep(int type)
+{
+  if (!beepEnabled || buzzerPin < 0)
+    return;
 
-  if (type == 1) {
-    tone(buzzerPin, 1500, 100);  // SELECT
-  } else {
-    tone(buzzerPin, 1000, 50);   // Scroll or back
+  if (type == 1)
+  {
+    tone(buzzerPin, 1500, 100);
+  }
+  else
+  {
+    tone(buzzerPin, 1000, 50);
   }
   delay(60);
   noTone(buzzerPin);
--- src/MenuUI.h
+++ src/MenuUI.h
@@ -3,41 +3,42 @@
 
 #include <Adafruit_SSD1306.h>
 
-class MenuUI {
-  public:
-    MenuUI(Adafruit_SSD1306* display, int up, int down, int select, int back, int buzzer);
-    void begin();
-    void setMenuItems(const char* items[], int count);
-    void update();
-    void drawMenu();
-    void setCallback(void (*callback)(int));
-    void setBackCallback(void (*callback)());
-    void setBeepEnabled(bool enabled);
-    void setBrightnessLevel(uint8_t level);
+class MenuUI
+{
+public:
+  MenuUI(Adafruit_SSD1306 *display, int up, int down, int select, int back, int buzzer);
+  void begin();
+  void setMenuItems(const char *items[], int count, bool resetSelection = true);
+  void update();
+  void drawMenu();
+  void setCallback(void (*callback)(int));
+  void setBackCallback(void (*callback)());
+  void setBeepEnabled(bool enabled);
+  void setBrightnessLevel(uint8_t level);
 
-  private:
-    Adafruit_SSD1306* display;
-    const char** menuItems;
+private:
+  Adafruit_SSD1306 *display;
+  const char **menuItems;
 
-    int selectedItem;
-    int topItem;
-    int itemCount;
-    int maxVisibleItems = 2;
+  int selectedItem;
+  int topItem;
+  int itemCount;
+  int maxVisibleItems = 2;
 
-    int buttonUpPin;
-    int buttonDownPin;
-    int buttonSelectPin;
-    int buttonBackPin;
-    int buzzerPin;
+  int buttonUpPin;
+  int buttonDownPin;
+  int buttonSelectPin;
+  int buttonBackPin;
+  int buzzerPin;
 
-    bool beepEnabled = true;
+  bool beepEnabled = true;
 
-    void (*callback)(int);
-    void (*backCallback)();
+  void (*callback)(int);
+  void (*backCallback)();
 
-    void moveUp();
-    void moveDown();
-    void beep(int type = 0);
+  void moveUp();
+  void moveDown();
+  void beep(int type = 0);
 };
 
 #endif
